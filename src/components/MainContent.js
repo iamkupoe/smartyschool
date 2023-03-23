ad08d1606ce661eeeca1c8973eee4324e5da5621
personal <samuelkupoe1@gmail.com>
2023-03-23 01:57:24 +0000

    updates in progress

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
 import "../css/MainContent.css";
 
 function MainContent() {
   return (
-    <div>
-      <h1>Welcome to my page</h1>
-      <p>This is some example text.</p>
+    <div className="grid-container">
+      <div className="phase1">
+        <div className="grid-tuition">
+          <h5 id="title">Expected</h5>
+          <p id="sub">{" GH\u20B5"}</p>
+          <h5 id="title">Paid</h5>
+          <p id="sub">{" GH\u20B5"}</p>
+          <h5 id="title">Balance</h5>
+          <p id="sub">{" GH\u20B5"}</p>
+        </div>
+        <div className="toiletories">
+          <h5>Expected</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Paid</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Balance</h5>
+          <p>{" GH\u20B5"}</p>
+        </div>
+      </div>
+      <div className="phase2">
+        <div className="exam">
+          <h5>Expected</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Paid</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Balance</h5>
+          <p>{" GH\u20B5"}</p>
+        </div>
+        <div className="feeding">
+          <h5>Expected</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Paid</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Balance</h5>
+          <p>{" GH\u20B5"}</p>
+        </div>
+      </div>
+      <div className="phase3">
+        <div className="transport">
+          <h5>Expected</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Paid</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Balance</h5>
+          <p>{" GH\u20B5"}</p>
+        </div>
+        <div className="enrolment">
+          <h5>Expected</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Paid</h5>
+          <p>{" GH\u20B5"}</p>
+          <h5>Balance</h5>
+          <p>{" GH\u20B5"}</p>
+        </div>
+      </div>
     </div>
   );
 }
