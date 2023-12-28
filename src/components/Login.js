dfa86ff772dd4e984ae958a03d9aeb0e8caf5329
Adams-ghub <bamfoadamsf@gmail.com>
2023-12-28 21:41:55 +0000

    Modify the component to use ES6 arrow functions

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import '../css/Login.css';
+import React from "react";
+import "../css/Login.css";
 import SchoolLogo from "../assets/images/Campion-College-School-Logo-Design-1024x791.jpeg";
 
-function Login() {
+const Login = () => {
   return (
     <div className="login-container">
-    <div className='image-container'>
-      <img src= {SchoolLogo} alt="Logo" className="logo" />
+      <div className="image-container">
+        <img src={SchoolLogo} alt="Logo" className="logo" />
       </div>
-      <div className='form-container'>
-      <form className="login-form">
-        <label id='titles'>
-          Username:
-          <input type="text" name="username" />
-        </label>
-        <br />
-        <label id='titles'>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <br />
-        <button id='sub' type="Login">Login</button>
-      </form>
+      <div className="form-container">
+        <form className="login-form">
+          <label id="titles">
+            Username:
+            <input type="text" name="username" />
+          </label>
+          <br />
+          <label id="titles">
+            Password:
+            <input type="password" name="password" />
+          </label>
+          <br />
+          <button id="sub" type="Login">
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
