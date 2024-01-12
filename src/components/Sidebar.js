e75778744d2d5560c0b78b0987d88c44f7d578e2
Adams-Ghub <bamfoadamsf@gmail.com>
2024-01-12 00:32:52 +0000

    Modify all buttons to link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,41 +7,43 @@ import { HiUserAdd } from 'react-icons/hi';
 import { MdPayment } from 'react-icons/md';
 import { TbReportAnalytics } from 'react-icons/tb';
 import { FiSettings } from 'react-icons/fi';
+import {Link} from 'react-router-dom'
+
 
 const Sidebar = () => {
   return (
    
       <div className="sidebarContainer">
         <div className="logo-container">
-          <img src={logo} alt="School Logo" className="logo" />
+          <img src={logo} alt="School Logo" className="school-logo" />
           <h2 id="sch-name">My School International</h2>
         </div>
         <ul className="itemContainer">
-          <a className="item1">
+          <Link to="/dashboard/summary" className="item1">
             <RxDashboard id="dashb-icon" />
             <span id="items">Dashboard</span>
-          </a>
-          <a className="item1">
+          </Link>
+          <Link  className="item1">
             <RiProfileFill id="dashb-icon" />
 
             <span id="items">Profile</span>
-          </a>
-          <di className="item1">
+          </Link>
+          <Link to="/dashboard/register" className="item1">
             <HiUserAdd id="dashb-icon" />
             <span id="items">Register</span>
-          </di>
-          <a className="item1">
+          </Link>
+          <Link to="/dashboard/fees" className="item1">
             <MdPayment id="dashb-icon" />
             <span id="items">Fees</span>
-          </a>
-          <a className="item1">
+          </Link>
+          <Link to="/dashboard/reports" className="item1">
             <TbReportAnalytics id="dashb-icon" />
             <span id="items">Reports</span>
-          </a>
-          <a className="item1">
+          </Link>
+          <Link to="/dashboard/settings" className="item1">
             <FiSettings id="dashb-icon" />
             <span id="items">Settings</span>
-          </a>
+          </Link>
         </ul>
       </div>
    
