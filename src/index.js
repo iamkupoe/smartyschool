fd5320e60b033c89a02bfdb279f0d0f55346296f
Adams-Ghub <bamfoadamsf@gmail.com>
2024-01-12 00:31:22 +0000

    Create the router in index.js file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,56 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Summary from './components/dashboard/Summary';
+import Register from './components/dashboard/Register';
+import Fees from './components/dashboard/Fees';
+import Reports from './components/dashboard/Reports';
+import Settings from './components/dashboard/Settings';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard />,
+    children: [
+      {
+        path: '',
+        element: <Summary />,
+      },
+      {
+        path: 'summary',
+        element: <Summary />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'fees',
+        element: <Fees />,
+      },
+      {
+        path: 'reports',
+        element: <Reports />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
