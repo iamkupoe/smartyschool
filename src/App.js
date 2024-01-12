93e852ab2077079f4f2efa22a91ffb1f67690abd
Adams-Ghub <bamfoadamsf@gmail.com>
2024-01-12 00:30:42 +0000

    Refactor App.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import Login from './components/Login';
-import MainContent from './components/MainContent';
-import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
 
-function App() {
+
+const App=()=> {
   return (
-    <div className="App">
-      <div className='toolsPanel'>
-        <Sidebar />
-      </div>
-      <div className='workspacePanel'>
-        <MainContent />
-      </div>
+    <div>
+      <Dashboard />
     </div>
   );
 }
