b0e91bd2e6fee479702dbc92a93b3efa3dc629ae
Adams-Ghub <bamfoadamsf@gmail.com>
2024-01-14 22:36:59 +0000

    Create the firebase file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0XVcwd_V1ExpjwH45IUR4-nEXVpO01OE",
+  authDomain: "smarty-school.firebaseapp.com",
+  projectId: "smarty-school",
+  storageBucket: "smarty-school.appspot.com",
+  messagingSenderId: "998035717703",
+  appId: "1:998035717703:web:91fcf9113e1c19ea44fb62"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
