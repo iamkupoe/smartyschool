be5902910e502d0740fc58ead345530cfacc87ef
Adams-Ghub <bamfoadamsf@gmail.com>
2024-01-14 22:30:54 +0000

    Add summary data as constant

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,8 +1,43 @@
+import { AiFillFileText  } from "react-icons/ai";
+import { FaBus,FaGraduationCap,FaToiletPaper  } from "react-icons/fa";
+import { MdFastfood } from "react-icons/md";
+
+
 export const summaryData = [
   {
-    name: 'Feeding fee',
+    name: 'Tuition fees',
     paid: '8,500',
     expected: '10,500',
     balance: '2,000',
+    icon:<FaGraduationCap color="#fff" size={50} />
   },
+  {
+    name: 'Transport fees',
+    paid: '8,500',
+    expected: '10,500',
+    balance: '2,000',
+    icon:<FaBus color="#fff" size={50} />
+  },
+  {
+    name: 'Exams fees',
+    paid: '8,500',
+    expected: '10,500',
+    balance: '2,000',
+    icon:<AiFillFileText color="#fff" size={50}  />
+  },
+  {
+    name: 'Feeding fees',
+    paid: '8,500',
+    expected: '10,500',
+    balance: '2,000',
+    icon:<MdFastfood color="#fff" size={50} />
+  },
+  {
+    name: 'Toiletories fee',
+    paid: '8,500',
+    expected: '10,500',
+    balance: '2,000',
+    icon:<FaToiletPaper color="#fff" size={50}  />
+  },
+  
 ];
