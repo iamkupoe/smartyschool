004f476ec9a8508976d5301ad0c693733b50bd32
Adams-Ghub <bamfoadamsf@gmail.com>
2024-02-08 19:36:01 +0000

    Modify the text for the fee components

--- src/components/dashboard/FeesComponent.jsx
+++ src/components/dashboard/FeesComponent.jsx
@@ -9,15 +9,15 @@ const FeesComponent = ({ icon, type, expected, balance, paid }) => {
       </div>
       <div className="bg-[#f2f2f2] w-[70%] px-[0.5rem]">
         <h2 className="text-[1rem] 2xl:text-[2rem] font-bold ">{type}</h2>
-        <p className="text-[#1338be] 2xl:text-[1.8rem]">
+        <p className="text-[#1338be] lg:text-[0.9rem] 2xl:text-[1.8rem]">
           <span className="font-bold">Expected:</span>
           <span className="">{'GH₵ ' + expected}</span>
         </p>
-        <p className="text-[#3cb043] 2xl:text-[1.5rem]">
+        <p className="text-[#3cb043]  lg:text-[0.9rem] 2xl:text-[1.5rem]">
           <span className="font-bold">Paid:</span>
           <span className="metrics-value"> {'GH₵ ' + paid}</span>
         </p>
-        <p className="text-[#b90e0a] 2xl:text-[1.5rem]">
+        <p className="text-[#b90e0a] lg:text-[0.9rem] 2xl:text-[1.5rem]">
           <span className="font-bold">Balance:</span>
           <span className="">{'GH₵ ' + balance}</span>
         </p>
