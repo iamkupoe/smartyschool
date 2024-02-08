a6f36e54f9995c1b8a1a3cff61de52744b1c0dee
Adams-Ghub <bamfoadamsf@gmail.com>
2024-02-08 19:22:25 +0000

    Complete the enrollment and attendance pages using tailwind css

--- src/components/dashboard/Summary.jsx
+++ src/components/dashboard/Summary.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../css/Summary.css';
-import { attendanceData, summaryData } from '../Constants';
+import { attendanceData, summaryData, enrollmentData } from '../Constants';
 import FeesComponent from './FeesComponent';
 import {
   BarChart,
@@ -18,12 +18,12 @@ import {
 } from 'recharts';
 
 function Summary() {
-  const COLORS = ['#0088FE', '#00C49F'];
-  return(
-    <div className="flex flex-col  h-[95vh] overflow-y-auto">
-      <div className="fees-container">
-        <h2>Fees payment</h2>
-        <div className="categories-box">
+  const COLOURS = ['#8884d8', '#00C49F'];
+  return (
+    <div className="flex flex-col  h-[95vh] overflow-y-auto p-[0.8rem] gap-[0.5rem]">
+      <div className="flex flex-col  border p-[0.5rem] border-[#c8c8c8]">
+        <h2 className='text-[#141414] 2xl:text-[2rem]'>Fees payment</h2>
+        <div className="flex flex-col gap-[0.8rem] sm:flex-row sm:flex-wrap">
           {summaryData.map((datum, index) => {
             return (
               <FeesComponent
@@ -37,26 +37,50 @@ function Summary() {
           })}
         </div>
       </div>
-      <div className="flex flex-col">
-        <div className="attendance-container">
-          <h2>Attendance</h2>
-          <div className='attendance-chart-box'>
+      <div className="flex flex-col md:flex-row gap-[0.5rem]">
+        <div className="md:w-[50%] border p-[0.5rem] border-[#c8c8c8]">
+          <h2 className='text-[#141414] 2xl:text-[2rem]'>Attendance</h2>
+          <div className="">
             {
-              <BarChart width={250} height={250} data={attendanceData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="boys" fill="#8884d8" width={10} />
-                <Bar dataKey="girls" fill="#82ca9d" width={10} />
-              </BarChart>
+              <ResponsiveContainer width="100%" height={300}>
+                <BarChart
+                  data={attendanceData}
+                  margin={{ top: 0, right: 0, left: 0, buttom: 0 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis domain={[0, 100]} tickCount={6} />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="Boys" fill="#1338be" width={10} />
+                  <Bar dataKey="Girls" fill="#3cb043" width={10} />
+                </BarChart>
+              </ResponsiveContainer>
             }
           </div>
         </div>
-        <div className="enrollment-container">
-          <h2>Attendance</h2>
-          {/* <div>{renderBarChart}</div> */}
+        <div className="md:w-[50%] border p-[0.5rem] border-[#c8c8c8]">
+          <h2 className='text-[#141414] 2xl:text-[2rem]'>Enrollment</h2>
+          <div className="">
+            {
+              <ResponsiveContainer width="100%" height={300}>
+                <PieChart>
+                  <Pie
+                    data={enrollmentData}
+                    cx="50%"
+                    cy={130}
+                    innerRadius={75}
+                    outerRadius={100}
+                    dataKey="value"
+                    label
+                  />
+
+                  <Tooltip />
+                  <Legend />
+                </PieChart>
+              </ResponsiveContainer>
+            }
+          </div>
         </div>
       </div>
     </div>
