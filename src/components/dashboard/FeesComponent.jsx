97e4e052921acd70cc2ba13191a356db88dbcb28
Adams-Ghub <bamfoadamsf@gmail.com>
2024-02-08 19:20:14 +0000

    Modify the styling with tailwind

--- src/components/dashboard/FeesComponent.jsx
+++ src/components/dashboard/FeesComponent.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import '../../css/FeesComponent.css';
 
-const FeesComponent = ({ icon, type,expected,balance,paid }) => {
+const FeesComponent = ({ icon, type, expected, balance, paid }) => {
   return (
-    <div className='fees-component-main-box'>
-      <div className='icon-box'>
-        {icon}       
+    <div className="flex flex-row w-[100%] sm:w-[48%] lg:w-[32%] xl:w-[24%]">
+      <div className="bg-[#fa8128] px-[0.8rem] w-[30%] flex flex-row justify-center items-center">
+        {icon}
       </div>
-      <div className='info-section'>
-      <h2 className='title-text'>{type}</h2>
-        <p className='expected-title-value-box'>
-          <span className='metrics-title'>Expected:</span>
-          <span className='metrics-value'>{expected}</span>
+      <div className="bg-[#f2f2f2] w-[70%] px-[0.5rem]">
+        <h2 className="text-[1rem] 2xl:text-[2rem] font-bold ">{type}</h2>
+        <p className="text-[#1338be] 2xl:text-[1.8rem]">
+          <span className="font-bold">Expected:</span>
+          <span className="">{'GH₵ ' + expected}</span>
         </p>
-        <p className='paid-title-value-box'>
-          <span className='metrics-title'>Paid:</span>
-          <span className='metrics-value'>{paid}</span>
+        <p className="text-[#3cb043] 2xl:text-[1.5rem]">
+          <span className="font-bold">Paid:</span>
+          <span className="metrics-value"> {'GH₵ ' + paid}</span>
         </p>
-        <p className='balance-title-value-box'>
-          <span className='metrics-title'>Balance:</span>
-          <span className='metrics-value'>{balance}</span>
+        <p className="text-[#b90e0a] 2xl:text-[1.5rem]">
+          <span className="font-bold">Balance:</span>
+          <span className="">{'GH₵ ' + balance}</span>
         </p>
       </div>
     </div>
