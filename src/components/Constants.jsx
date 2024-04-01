092ae021168c56c39ce840974dcb3484e9d4f331
Adams-Ghub <bamfoadamsf@gmail.com>
2024-04-01 12:08:38 +0000

    Add class and registrations arrays

--- src/components/Constants.jsx
+++ src/components/Constants.jsx
@@ -8,7 +8,9 @@ export const summaryData = [
     paid: '8,500',
     expected: '10,500',
     balance: '2,000',
-    icon: <FaGraduationCap className="text-[#ffffff] text-[50px] 2xl:text-[80px]" />,
+    icon: (
+      <FaGraduationCap className="text-[#ffffff] text-[50px] 2xl:text-[80px]" />
+    ),
   },
   {
     name: 'Transport fees',
@@ -22,7 +24,9 @@ export const summaryData = [
     paid: '8,500',
     expected: '10,500',
     balance: '2,000',
-    icon: <AiFillFileText className="text-[#ffffff] text-[50px] 2xl:text-[80px]" />,
+    icon: (
+      <AiFillFileText className="text-[#ffffff] text-[50px] 2xl:text-[80px]" />
+    ),
   },
   {
     name: 'Feeding fees',
@@ -36,7 +40,9 @@ export const summaryData = [
     paid: '8,500',
     expected: '10,500',
     balance: '2,000',
-    icon: <FaToiletPaper className="text-[#ffffff] text-[50px] 2xl:text-[80px]" />,
+    icon: (
+      <FaToiletPaper className="text-[#ffffff] text-[50px] 2xl:text-[80px]" />
+    ),
   },
 ];
 
@@ -49,6 +55,154 @@ export const attendanceData = [
 ];
 
 export const enrollmentData = [
-  { name: 'Boys', value: 60, fill:'#1338be' },
-  { name: 'Girls', value: 44, fill:'#3cb043'},
+  { name: 'Boys', value: 60, fill: '#1338be' },
+  { name: 'Girls', value: 44, fill: '#3cb043' },
+];
+
+export const classArray = [
+  { name: 'Creche', value: 'Creche' },
+  { name: 'Nursery 1', value: 'Nursery 1' },
+  { name: 'Nursery 2', value: 'Nursery 2' },
+  { name: 'KG 1', value: 'KG 1' },
+  { name: 'KG 2', value: 'KG 2' },
+  { name: 'Basic 1', value: 'Basic 1' },
+  { name: 'Basic 2', value: 'Basic 2' },
+  { name: 'Basic 3', value: 'Basic 3' },
+  { name: 'Basic 4', value: 'Basic 4' },
+  { name: 'Basic 5', value: 'Basic 5' },
+  { name: 'Basic 6', value: 'Basic 6' },
+  { name: 'JHS1', value: 'JHS1' },
+  { name: 'JHS2', value: 'JHS2' },
+  { name: 'JHS3', value: 'JHS3' },
+];
+
+export const Registrations = [
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2202241005',
+    surname: 'Sessi',
+    otherNames: 'Xavier',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240917',
+    surname: 'Duker',
+    otherNames: 'Claudia',
+    stage: 'Basic 4',
+    transport: true,
+    status: 'regular',
+  },
+  {
+    id: '2302240907',
+    surname: 'Boateng',
+    otherNames: 'Caleb Osei',
+    stage: 'JHS 2',
+    transport: true,
+    status: 'scholarship',
+  },
+  {
+    id: '2302240924',
+    surname: 'Boateng',
+    otherNames: 'Cara Amissah',
+    stage: 'JHS 2',
+    transport: true,
+    status: 'scholarship',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
+  {
+    id: '2302240927',
+    surname: 'Nhyiraba',
+    otherNames: 'Serwaa Danquah',
+    stage: 'Basic 4',
+    transport: false,
+    status: 'regular',
+  },
 ];
