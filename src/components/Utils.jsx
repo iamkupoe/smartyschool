846470632c055fd1d618a76b668d88895d724ae3
Adams-Ghub <bamfoadamsf@gmail.com>
2024-04-01 12:09:47 +0000

    Create getRegisterCurrentScreen

--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -1,21 +1,38 @@
 export const getCurrentUrl = (url) => {
   const current = url.slice(11, location.pathname.length);
-  switch (current) {
-    case '':
-      return 'summary';
-    case 'summary':
-      return 'summary';
-    case 'profile':
-      return 'profile';
-    case 'fees':
-      return 'fees';
-    case 'reports':
-      return 'reports';
-    case 'settings':
-      return 'settings';
-    case 'register':
-      return 'register';
-    default:
-      break;
+  let displayUrl = 'summary';
+
+  if (current === '' || current === 'summary') {
+    displayUrl = 'summary';
+  } else if (current === 'profile') {
+    displayUrl = 'profile';
+  } else if (current === 'profile') {
+    displayUrl = 'profile';
+  } else if (current === 'fees') {
+    displayUrl = 'fees';
+  } else if (current === 'reports') {
+    displayUrl = 'reports';
+  } else if (current === 'settings') {
+    displayUrl = 'settings';
+  } else if (
+    current === 'register' ||
+    current === 'register/registrations' ||
+    'register/registration-form'
+  ) {
+    displayUrl = 'register';
   }
+  return displayUrl;
+};
+export const getRegisterCurrentScreen = (url) => {
+  const current = url.slice(20, location.pathname.length);
+  let displayUrl = 'registration';
+
+  if (current === '' || current === 'registration') {
+    displayUrl = 'registration';
+  } else if (current === 'registration-form') {
+    displayUrl = 'registration-form';
+  } else if (current === 'register-tutor') {
+    displayUrl = 'register-tutor';
+  }
+  return displayUrl;
 };
