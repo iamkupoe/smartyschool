cd900dfeb89aa3d37df9f713be70e79838afcffb
Adams-Ghub <bamfoadamsf@gmail.com>
2024-10-01 21:30:25 +0000

    Work on filter registration by class function

--- src/components/tutor_dashboard/attendance/MarkAttendance.jsx
+++ src/components/tutor_dashboard/attendance/MarkAttendance.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Registrations, classArray } from '../../Constants';
 
 const MarkAttendance = () => {
@@ -6,36 +6,57 @@ const MarkAttendance = () => {
   const [filteredClass, setFilteredClass] = useState([]);
   const [markDate, setMarkDate] = useState('');
   const textInputStyle =
-    'border border-[#525252] rounded-sm px-[0.2rem] py-[0.1rem] w-full outline-none';
+    'border border-[#525252] rounded-sm px-[0.2rem] py-[0.1rem] w-[100%] outline-none';
   const titleInputContainer = 'mb-[0.5rem]';
   const titleText = 'font-bold w-full';
 
   const filterRegistrationByClass = () => {
-    const filtered = Registrations.filter(
-      (students) => students.stage === stage
+    const masterChkBox = document.getElementsByClassName('master_chkbox');
+
+    if (masterChkBox[0].checked) {
+      masterChkBox[0].checked = false;
+    }
+    setFilteredClass(
+      Registrations.filter((students) => students.stage === stage)
     );
-    setFilteredClass(filtered);
   };
 
   const selectAllStudents = () => {
-    const studentList = Array.from(document.getElementsByClassName('chkBox'));
+    const studentList = Array.from(
+      document.getElementsByClassName('inputListItem')
+    ).map((child) => {
+      return child.children[0];
+    });
     const masterChkBox = document.getElementsByClassName('master_chkbox');
-    console.log('masterCheckBox:', masterChkBox);
+
     studentList.map((divElement) => {
-      divElement.checked = !masterChkBox[0].checked;
+      divElement;
+      divElement.checked = masterChkBox[0].checked ? true : false;
     });
   };
 
+  const uncheckMainChkBox = (e) => {
+    const masterChkBox = document.getElementsByClassName('master_chkbox');
+    if (e.target.checked === false) {
+      masterChkBox.checked = false;
+    }
+  };
+
+  useEffect(() => {
+    filterRegistrationByClass();
+  }, [stage]);
+
   return (
-    <div className={`mx-[0.75rem] my-[1rem]`}>
-      <div>
+    <div
+      className={`mx-[0.75rem] my-[1rem] flex flex-col sm:mx-[6rem] md:flex-row md:mx-[1.5rem] md:justify-between md:gap-[1rem]`}
+    >
+      <div className={` md:w-[50%]`}>
         <div className={titleText}>Class</div>
         <select
           className={textInputStyle}
           value={stage}
           onChange={(e) => {
             setStage(e.target.value);
-            filterRegistrationByClass();
           }}
           name=""
           id="class"
@@ -48,7 +69,7 @@ const MarkAttendance = () => {
             );
           })}
         </select>
-        <div className={`${titleInputContainer} md:w-[30%]`}>
+        <div className={`${titleInputContainer} md:w-[50%]`}>
           <div className={titleText}>Date</div>
           <input
             type="date"
@@ -56,18 +77,18 @@ const MarkAttendance = () => {
             value={markDate}
             onChange={(e) => setMarkDate(e.target.value)}
             id="dateInputId"
-            className={textInputStyle}
+            className={`border border-[#525252] rounded-sm px-[0.2rem] py-[0.1rem] w-[100%] md:w-[200%] outline-none`}
           />
         </div>
       </div>
-      <div>
-        <section>
+      <div className={`md:w-[50%]`}>
+        <section className="width-[100%">
           <div className={`flex flex-row justify-between`}>
             <div className={titleText}>Student's name</div>
             <div className={`w-[20%] text-center font-bold`}>Status</div>
           </div>
           <div
-            className={`flex flex-row justify-end ${
+            className={`flex flex-row justify-end p-[0.2rem] bg-[#eeeeee] ${
               filteredClass.length <= 0 ? 'hidden' : ''
             }`}
           >
@@ -86,10 +107,19 @@ const MarkAttendance = () => {
           {filteredClass.length > 0 ? (
             filteredClass.map((student, index) => {
               return (
-                <div className={`flex flex-row justify-between`}>
+                <div
+                  className={`flex flex-row justify-between p-[0.2rem] ${
+                    index % 2 > 0 ? 'bg-[#eeeeee]' : ''
+                  } `}
+                >
                   <div> {student.surname + ' ' + student.otherNames} </div>
-                  <div className={`w-[20%] text-center`}>
-                    <input type="checkbox" name="all" className="chkBox" />
+                  <div className={`w-[20%] text-center inputListItem`}>
+                    <input
+                      type="checkbox"
+                      name="all"
+                      // className="chkBox"
+                      onClick={(e) => uncheckMainChkBox(e)}
+                    />
                   </div>
                 </div>
               );
@@ -98,6 +128,14 @@ const MarkAttendance = () => {
             <div>No reqistered student in this class</div>
           )}
         </section>
+        {filteredClass.length > 0 ? (
+          <input
+            type="submit"
+            className="bg-[#0073cf] my-[0.5rem] text-[#fff] text-[1rem] lg:text-[1.5rem] p-[0.25rem] lg:p-[0.3rem] w-full  rounded-md outline-none"
+            value="Save"
+            name="Save"
+          />
+        ) : null}
       </div>
     </div>
   );
