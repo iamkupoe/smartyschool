23458857b06eed6dab81c760ff906a9e5d8ddf10
Adams-Ghub <bamfoadamsf@gmail.com>
2024-10-04 23:05:09 +0000

    Add the subject drop down to the record performance page on mobile version

--- src/components/tutor_dashboard/performance/RecordPerformance.jsx
+++ src/components/tutor_dashboard/performance/RecordPerformance.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { classArray, Registrations } from '../../Constants';
+import {
+  classArray,
+  Registrations,
+  primary,
+  jhs,
+  preschool,
+} from '../../Constants';
 import Select from 'react-select';
 
 const RecordPerformance = () => {
   const [stage, setStage] = useState('');
+  const [subjects, setSubjects] = useState('');
+  const [subject, setSubject] = useState('');
   const [selectedStudent, setSelectedStudent] = useState('');
   const [filteredClass, setFilteredClass] = useState('');
   const customStyles = {
@@ -21,15 +29,27 @@ const RecordPerformance = () => {
 
   useEffect(() => {
     setFilteredClass(
-      Registrations.filter((reg) => reg.stage === stage).map(
-        (filtrate) => {
-          return {
-            value: `${filtrate.otherNames} ${filtrate.surname}`,
-            label: `${filtrate.otherNames} ${filtrate.surname}`,
-          };
-        }
-      )
+      Registrations.filter((reg) => reg.stage === stage).map((filtrate) => {
+        return {
+          value: `${filtrate.otherNames} ${filtrate.surname}`,
+          label: `${filtrate.otherNames} ${filtrate.surname}`,
+        };
+      })
     );
+    if (
+      stage === 'Basic 1' ||
+      stage === 'Basic 2' ||
+      stage === 'Basic 3' ||
+      stage === 'Basic 4' ||
+      stage === 'Basic 5' ||
+      stage === 'Basic 6'
+    ) {
+      setSubjects(primary);
+    } else if (stage === 'JHS 1' || stage === 'JHS 2' || stage === 'JHS 3') {
+      setSubjects(jhs);
+    } else {
+      setSubjects(preschool);
+    }
   }, [stage]);
 
   return (
@@ -66,7 +86,29 @@ const RecordPerformance = () => {
             placeholder="Enter student name to search"
           />
         </div>
+        <div>
+          <div>Subject</div>
+          <select
+            className={textInputStyle}
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+            name=""
+            id="class"
+          >
+            {subjects &&
+              subjects.map((thesub, index) => {
+                return (
+                  <option value={thesub} key={index}>
+                    {thesub}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
       </section>
+      <section></section>
     </main>
   );
 };
