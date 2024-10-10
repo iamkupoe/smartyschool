b42e6175eb820f43e185f7999d46c9f9dfc54ff2
Adams-Ghub <bamfoadamsf@gmail.com>
2024-10-10 00:04:04 +0000

    Modify the delete button of MarkCard

--- src/components/tutor_dashboard/performance/MarksCard.jsx
+++ src/components/tutor_dashboard/performance/MarksCard.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 
 const MarksCard = ({ title, actual_mrk, total_mrk }) => {
   const [actualmrk, setActualMrk] = useState(actual_mrk);
   const [totalMrk, setTotalMark] = useState(total_mrk);
 
-  const inputStyle = 'w-[2rem]';
+  const inputStyle =
+    'w-[2.5rem] bg-transparent border-solid border border-[#212529] outline-none px-[0.3rem]';
   return (
     <div className="bg-[#EBEBEB] p-[5px] my-[0.5rem]">
-      <div>{title}</div>
+      <div className="flex flex-row justify-between">
+        <div>{title}</div>
+        <div type="button" className="bg-transparent outline-none">
+          <MdDelete className="text-[#d00000] text-[1.2rem]" />
+        </div>
+      </div>
       <div className="flex row">
         <div>Marks:</div>
         <input
