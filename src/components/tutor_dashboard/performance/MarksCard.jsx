d6f3a2744acbd8f56568d0940fa9507a3fb8bbdd
Adams-Ghub <bamfoadamsf@gmail.com>
2025-03-16 11:46:10 +0000

    Develop the marks cards

--- src/components/tutor_dashboard/performance/MarksCard.jsx
+++ src/components/tutor_dashboard/performance/MarksCard.jsx
@@ -8,7 +8,7 @@ const MarksCard = ({ title, actual_mrk, total_mrk }) => {
   const inputStyle =
     'w-[2.5rem] bg-transparent border-solid border border-[#212529] outline-none px-[0.3rem]';
   return (
-    <div className="bg-[#EBEBEB] p-[5px] my-[0.5rem]">
+    <div className="bg-[#EBEBEB] p-[5px] my-[0.5rem] w-[99%] sm:w-[48%] ">
       <div className="flex flex-row justify-between">
         <div>{title}</div>
         <div type="button" className="bg-transparent outline-none">
